0608abae37e6ccec3a00a62b960d52257022d938
tobwa-on <tobiaswawak@online.de>
2024-11-14 02:10:07 +0100

    delete unused files from auch

--- src/main/java/edu/dhbw/mos/fim/pakt/startup/CreateUsers.java
+++ src/main/java/edu/dhbw/mos/fim/pakt/startup/CreateUsers.java
@@ -24,10 +24,6 @@ public class CreateUsers
 		if (userRepo.count() == 0)
 		{
 			userRepo.createAndPersist("admin", "on23", Set.of("user", "admin"));
-			userRepo.createAndPersist("tobias", "on23", Set.of("user"));
-			userRepo.createAndPersist("test", "on23", Set.of("user", "admin"));
-			userRepo.createAndPersist("test2", "on23", Set.of("user", "admin"));
-
 		}
 	}
 }
--- src/test/java/edu/dhbw/mos/fim/pakt/rest/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package edu.dhbw.mos.fim.pakt.rest;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
